8944b523387ca455513dbaee84bd6bfa5ff7893b
kitso1999 <kitso1999@gmail.com>
2020-10-13 02:02:13 +0400

    Game Timing and User Input Implemented

--- ConsoleTetris/Source.cpp
+++ ConsoleTetris/Source.cpp
@@ -1,51 +1,59 @@
 #include <iostream>
+#include <thread>
 
 #include <Windows.h>
 
-
+using namespace std;
 
 //Game Properties
 const int BoardWidth = 10;
 const int BoardHeight = 20;
 
 //Main Game Objects*****************
-const char* tetrominoes[7] = 
+const wchar_t* tetrominoes[7] = 
 {
-	"..X."
-	"..X."
-	"..X."
-	"..X.",
+	L"..X."
+	L"..X."
+	L"..X."
+	L"..X.",
 
-	".X.."
-	".XX."
-	".X.."
-	"....",
+	L".X.."
+	L".XX."
+	L".X.."
+	L"....",
 
-	"...."
-	".XX."
-	".XX."
-	"....",
+	L"...."
+	L".XX."
+	L".XX."
+	L"....",
 
-	".X.."
-	".X.."
-	".XX."
-	"....",
+	L".X.."
+	L".X.."
+	L".XX."
+	L"....",
 
-	"..X."
-	"..X."
-	".XX."
-	"....",
+	L"..X."
+	L"..X."
+	L".XX."
+	L"....",
 
-	"..X."
-	".XX."
-	".X.."
-	"....",
+	L"..X."
+	L".XX."
+	L".X.."
+	L"....",
 
-	".X.."
-	".XX."
-	"..X."
-	"...."
+	L".X.."
+	L".XX."
+	L"..X."
+	L"...."
 };
+
+
+const wchar_t* assets = L" ABCDEFG=#";
+//**********************************
+
+//Game State Variables
+bool bkey[4];
 //**********************************
 
 int Rotate(int x, int y, int r)
@@ -66,5 +74,22 @@ bool DoesPieceFit(int r)
 
 int main()
 {
-	
+
+	//Game Logic
+	bool GameOver = false;
+	while (!GameOver)
+	{
+
+		//Game Timing
+		std::this_thread::sleep_for(50ms);
+
+		//*****************************
+
+		//User Input
+		for (int i = 0; i < 4; ++i)							//	R	L  D Z
+			bkey[i] = ((0x0001) & GetAsyncKeyState((const char)"\x27\x25\x28Z"[i]));
+
+		//*****************************
+	}
+	//*********************************
 }
